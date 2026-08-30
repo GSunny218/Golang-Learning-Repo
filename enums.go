df6419b7a282b5e9206ad7371b74519a118da2ad
Sunny-Gupta2 <sunny.gupta.5062644@ves.ac.in>
2026-08-30 10:10:46 +0530

    completed with enums

---
+++ enums.go
@@ -0,0 +1,32 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// type OrderStatus int;
+// const (
+// 	Received OrderStatus = iota;
+// 	Confirmed;
+// 	Prepared;
+// 	Delivered;
+// );
+
+// type OrderStatus string
+
+// const (
+// 	Received  OrderStatus = "received";
+// 	Confirmed             = "confirmed";
+// 	Prepared              = "prepared";
+// 	Delivered             = "delivered";
+// )
+
+// func changeOrderStatus(status OrderStatus) {
+// 	fmt.Println("Changing order status to ", status)
+// }
+// func main() {
+// 	changeOrderStatus(Received)  // 0
+// 	changeOrderStatus(Confirmed) // 1
+// 	changeOrderStatus(Prepared)  // 2
+// 	changeOrderStatus(Delivered) // 3
+// }

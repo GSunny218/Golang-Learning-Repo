e06868aceba75eec1ca9ed6590e3605281cfa75f
Sunny-Gupta2 <sunny.gupta.5062644@ves.ac.in>
2026-09-12 13:52:23 +0530

    parse config file

--- STUDENTS-API/internal/config/config.go
+++ STUDENTS-API/internal/config/config.go
@@ -1,4 +1,40 @@
-package config;
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string
+}
 type Config struct {
-	Env string
+	Env          string `yaml: "env" env: ""ENV env-required: "true" env-default: "production"` //Struct tag
+	StoragePatch string `yaml: "storage_path" env-required: "true"`
+	HTTPServer   `yaml: "http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		configFlag := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+		configPath = *configFlag
+		if configPath == "" {
+			log.Fatal("Config path is not set")
+		}
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatal("Config file does not exist: %s", configPath)
+	}
+	var cfg Config
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatal("Failed to read config file: %s", err.Error())
+	}
+	return &cfg
 }
